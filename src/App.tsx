7057f7bcdc24cd1a162e91573403d0bf75a929e7
Jerome Thayananthajothy <tjthavarshan@gmail.com>
2025-11-11 10:05:50 +0530

    refactor: simplify routing structure and remove unnecessary login route

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 import ProtectedRoute from "./components/ProtectedRoute";
 import Login from "./pages/Login";
@@ -9,24 +9,15 @@ function App() {
     <AuthProvider>
       <BrowserRouter>
         <Routes>
-          <Route
-            path="/"
-            element={
-              <ProtectedRoute requireOAuthRedirect={true}>
-                <Login />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/login"
-            element={
-              <ProtectedRoute requireOAuthRedirect={true}>
-                <Login />
-              </ProtectedRoute>
-            }
-          />
           <Route path="/unauthorized" element={<Unauthorized />} />
-          <Route path="*" element={<Navigate to="/unauthorized" replace />} />
+          <Route
+            path="*"
+            element={
+              <ProtectedRoute requireOAuthRedirect={true}>
+                <Login />
+              </ProtectedRoute>
+            }
+          />
         </Routes>
       </BrowserRouter>
     </AuthProvider>
